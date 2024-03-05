28c0bb9994c59c91e62d04ac3a8fb3290665bdd1
JEFF Liao <jeffliao92316046@gmail.com>
2024-03-05 20:25:37 +0800

    owo

--- main.go
+++ main.go
@@ -1,15 +1,11 @@
 package main
 
 import (
-	//"github.com/gin-gonic/gin"
-	"fmt"
 	"helloworld/routers/api"
 )
 
 func main() {
-	data := []api.Data_structure{}
-	fmt.Print("start")
-	api.GET_testdata(data)
+	api.GET_testdata()
 }
 
 /*
--- routers/api/GET_testdata.go
+++ routers/api/GET_testdata.go
@@ -11,25 +11,27 @@ type Data_structure struct {
 	id     int
 	stock  string
 	date   string
-	number int
-	people int
-	share  int
+	number string
+	people string
+	share  string
 }
 
-func GET_testdata(data []Data_structure) {
-	connect_sql(data)
+func GET_testdata() {
+	connect_sql()
 }
 
-func connect_sql(data []Data_structure) {
+func connect_sql() {
+	data := []Data_structure{}
 	db, err := sql.Open("sqlite3", "./db.sqlite3")
 	checkErr(err)
 	defer db.Close()
-	rows, err := db.Query("select * from data")
+	rows, err := db.Query("select * from data ")
 	checkErr(err)
 	for i := 0; rows.Next(); i++ {
-		err := rows.Scan(data[i].id, data[i].stock, data[i].date, data[i].number, data[i].people, data[i].share)
+		data = append(data, Data_structure{})
+		err := rows.Scan(&(data[i].id), &(data[i].stock), &(data[i].date), &(data[i].number), &(data[i].people), &(data[i].share))
 		checkErr(err)
-		fmt.Printf("%d", data[i].id)
+		fmt.Printf("%d %s %s %2s %9s %11s\n", data[i].id, data[i].stock, data[i].date, data[i].number, data[i].people, data[i].share)
 	}
 }
 
